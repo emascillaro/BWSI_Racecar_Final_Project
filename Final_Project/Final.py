fca600673592f9d0e6c8041d24b7ec2e92973d80
emascillaro <emascillaro@gmail.com>
2020-07-27 11:58:42 -0400

    Final Project

---
+++ Final_Project/Final.py
@@ -0,0 +1,135 @@
+"""
+Copyright MIT and Harvey Mudd College
+MIT License
+Summer 2020
+
+Final Project
+"""
+
+########################################################################################
+# Imports
+########################################################################################
+
+import sys
+import cv2 as cv
+import numpy as np
+
+sys.path.insert(1, "../../library")
+import racecar_core
+import racecar_utils as rc_utils
+
+from enum import IntEnum
+
+class State(IntEnum):
+    start = 0
+    line_following = 1
+    cone slaloming = 2
+    wall_parking = 3
+
+########################################################################################
+# Global variables
+########################################################################################
+
+rc = racecar_core.create_racecar()
+
+# >> Constants
+# The smallest contour we will recognize as a valid contour
+MIN_CONTOUR_AREA = 30
+
+# A crop window for the floor directly in front of the car
+CROP_FLOOR = ((360, 0), (rc.camera.get_height(), rc.camera.get_width()))
+
+# Colors, stored as a pair (hsv_min, hsv_max)
+BLUE = ((90, 50, 50), (120, 255, 255))  # The HSV range for the color blue
+# TODO (challenge 1): add HSV ranges for other colors
+
+# >> Variables
+speed = 0.0  # The current speed of the car
+angle = 0.0  # The current angle of the car's wheels
+contour_center = None  # The (pixel row, pixel column) of contour
+contour_area = 0  # The area of contour
+
+########################################################################################
+# Functions
+########################################################################################
+
+
+########################################################################################
+# Functions
+########################################################################################
+
+
+def update_contour():
+    """
+    Finds contours in the current color image and uses them to update contour_center
+    and contour_area
+    """
+    global contour_center
+    global contour_area
+
+    image = rc.camera.get_color_image()
+
+    if image is None:
+        contour_center = None
+        contour_area = 0
+    else:
+        # TODO (challenge 1): Search for multiple tape colors with a priority order
+        # (currently we only search for blue)
+
+        # Crop the image to the floor directly in front of the car
+        image = rc_utils.crop(image, CROP_FLOOR[0], CROP_FLOOR[1])
+
+        # Find all of the blue contours
+        contours = rc_utils.find_contours(image, BLUE[0], BLUE[1])
+
+        # Select the largest contour
+        contour = rc_utils.get_largest_contour(contours, MIN_CONTOUR_AREA)
+
+        if contour is not None:
+            # Calculate contour information
+            contour_center = rc_utils.get_contour_center(contour)
+            contour_area = rc_utils.get_contour_area(contour)
+
+            # Draw contour onto the image
+            rc_utils.draw_contour(image, contour)
+            rc_utils.draw_circle(image, contour_center)
+
+        else:
+            contour_center = None
+            contour_area = 0
+
+        # Display the image to the screen
+        rc.display.show_color_image(image)
+
+
+def start():
+    """
+    This function is run once every time the start button is pressed
+    """
+    global speed
+    global angle
+
+    # Initialize variables
+    speed = 0
+    angle = 0
+
+    # Set initial driving speed and angle
+    rc.drive.set_speed_angle(speed, angle)
+
+
+
+def update():
+    """
+    After start() is run, this function is run every frame until the back button
+    is pressed
+    """
+    global speed
+    global angle
+
+########################################################################################
+# DO NOT MODIFY: Register start and update and begin execution
+########################################################################################
+
+if __name__ == "__main__":
+    rc.set_start_update(start, update, update_slow)
+    rc.go()
